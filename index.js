a78757d58477ba31091226be4728d92b74fc3428
Margaret <eutopia@gmail.com>
2016-10-03 14:41:15 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,12 @@
+function receivesAFunction(callback) {
+	callback();
+}
+
+function returnsANamedFunction() {
+	return function named() {
+	};
+}
+
+function returnsAnAnonymousFunction() {
+	return () => {};
+}
